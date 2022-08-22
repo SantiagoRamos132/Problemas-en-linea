b918e83133accd4888ec15571ffbd804133b5bec
Santiago Ramos <104105694+SantiagoRamos132@users.noreply.github.com>
2022-08-22 14:40:15 -0600

    Update Beecrowd - 1871- Zero means Zero.py

--- Beecrowd - 1871- Zero means Zero.py
+++ Beecrowd - 1871- Zero means Zero.py
@@ -4,18 +4,24 @@ Beecrowd
 Problema 1871 Zero means Zero
 Link: https://www.beecrowd.com.br/judge/es/problems/view/1871
 Autor: Ricardo Martins
-La idea de la solucion es leer dos enteros, enviar la suma de los dos enteros como un string a la funcion quitaceros, la cual retorna el mismo string pero sin ceros
-despues imprimir este nuevo string. El programa se detiene cuando los dos enteros que lee son dos ceros.
+La idea de la solucion es leer dos enteros, enviar la suma de los dos enteros a la funcion quitaceros e imprimir el resultado. 
+El programa se detiene cuando los dos enteros son 0.
+La idea del while de la funcion quitaceros es ver si el ultimo digito del entero recibido es 0, de no serlo, agregarlo a la variable de respuesta poniendolo una cifra
+a la izquierda de esta variable, luego dividir entero entre 10 para repetir el proceso con otro digito de entero.
 
 """
 
 
-def quitaceros(string):
-    resp = ""
-    for i in string:
-        if i !="0":
-            resp+=i
+def quitaceros(entero):
+    resp = 0
+    cont = 1
+    while entero!=0:
+        if entero%10!=0:
+            resp+=entero%10*cont
+            cont*=10
+        entero//=10   
     return resp
+
 while True:
     a,b = map(int,input().split())
     if a ==0 and b ==0:
