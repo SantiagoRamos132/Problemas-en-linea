1a44ddb36d094d9e20bc6629c060139e9161eb36
Santiago Ramos <104105694+SantiagoRamos132@users.noreply.github.com>
2022-09-02 10:26:25 -0600

    Update Beecrowd - 1006 - Promedio 2.py

--- Beecrowd - 1006 - Promedio 2.py
+++ Beecrowd - 1006 - Promedio 2.py
@@ -2,7 +2,8 @@
 Beecrowd
 Problema 1006 Promedio 2
 Link:https://www.beecrowd.com.br/judge/es/problems/view/1006
-El programa lee tres flotantes e imprime el promedio ponderado de los tres flotantes con 1 decimal. Este promedio se obtiene dividiendo la suma de los flotantes multiplicados por su peso entre la suma de todos los pesos
+El programa lee tres flotantes e imprime el promedio ponderado de los tres flotantes con 1 decimal. Este promedio se obtiene con una proporción de los valores
+ganados versus el total de valores posible.
 """
 A = float(input())
 B = float(input())
